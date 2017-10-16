9f1dd5bdc62e00b88a812ee4a60b700e3e8ed096
Brinick Simmons <brinick.simmons@gmail.com>
2017-10-16 16:33:32 +0200

    Add PYTHONPATH for protobuf

--- protobuf.sh
+++ protobuf.sh
@@ -8,7 +8,8 @@ build_requires:
  - "GCC-Toolchain:(?!osx)"
 prefer_system_check: |
    printf "#include <google/protobuf/stubs/common.h>\n#if (GOOGLE_PROTOBUF_VERSION < 0x002006001)\n#error \"System protobuf cannot be used, please install a version >= 2.6.1\"\n#endif\n" | cc -xc++ - -c -o /dev/null && protoc --version
-
+prepend_path:
+  PYTHONPATH: $PROTOBUF_ROOT/lib64/python2.7/site-packages:$PROTOBUF_ROOT/lib/python2.7/site-packages
 ---
 
 rsync -av --delete --exclude="**/.git" $SOURCEDIR/ .
@@ -36,4 +37,5 @@ setenv PROTOBUF_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path LD_LIBRARY_PATH \$::env(PROTOBUF_ROOT)/lib
 $([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(PROTOBUF_ROOT)/lib")
 prepend-path PATH \$::env(PROTOBUF_ROOT)/bin
+prepend-path PYTHONPATH \$::env(PROTOBUF_ROOT)/lib64/python2.7/site-packages:\$::env(PROTOBUF_ROOT)/lib/python2.7/site-packages
 EoF
