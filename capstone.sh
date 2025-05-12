3c7692ffd70684cb2a1a3ea4ef362aa15fd11ce2
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-05-12 09:04:02 +0200

    Bump capstone, add pkg-config support

--- capstone.sh
+++ capstone.sh
@@ -1,13 +1,17 @@
 package: capstone
-version: "4.0.2"
+version: "5.0.6"
 requires:
   - "GCC-Toolchain:(?!osx)"
 build_requires:
   - CMake
   - alibuild-recipe-tools
+  - ninja
 source: https://github.com/aquynh/capstone
+prepend_path:
+  PKG_CONFIG_PATH: "$CAPSTONE_ROOT/lib/pkgconfig"
 ---
 cmake $SOURCEDIR                          \
+      -G Ninja                            \
       -DCAPSTONE_ARCHITECUTRE_DEFAULT=OFF \
       -DCAPSTONE_BUILD_SHARED=OFF         \
       -DCMAKE_INSTALL_LIBDIR=lib          \
