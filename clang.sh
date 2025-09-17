51002bd973b340620b1e21dd8a003cfdc88eab93
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-09-17 10:38:39 +0200

    Improve relocation of clang

--- clang.sh
+++ clang.sh
@@ -109,13 +109,13 @@ rm "$INSTALLROOT"/lib/cmake/clang/*.bak
 # we want to resolve the environment when we run, not when we build this, to avoid
 # relocation issues in case GCC and clang are not built at the same time.
 if [ ! "X$GCC_TOOLCHAIN_ROOT" = X ]; then
-  cat > "$INSTALLROOT/bin-safe/$(clang --print-target-triple)-clang++.cfg" << \EOF
+  cat > "$INSTALLROOT/bin-safe/$($INSTALLROOT/bin-safe/clang --print-target-triple)-clang++.cfg" << EOF
 --gcc-toolchain=$GCC_TOOLCHAIN_ROOT
 EOF
-  cat > "$INSTALLROOT/bin-safe/$(clang --print-target-triple)-clang.cfg" << \EOF
+  cat > "$INSTALLROOT/bin-safe/$($INSTALLROOT/bin-safe/clang --print-target-triple)-clang.cfg" << EOF
 --gcc-toolchain=$GCC_TOOLCHAIN_ROOT
 EOF
-  cat > "$INSTALLROOT/bin-safe/$(clang --print-target-triple)-clang-cpp.cfg" << \EOF
+  cat > "$INSTALLROOT/bin-safe/$($INSTALLROOT/bin-safe/clang --print-target-triple)-clang-cpp.cfg" << EOF
 --gcc-toolchain=$GCC_TOOLCHAIN_ROOT
 EOF
 fi
